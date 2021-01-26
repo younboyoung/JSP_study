293c8b7fab48e2930370fff9d844d99a15b75f3d
DESKTOP-SJL8IB0\User <boyung1021@gmail.com>
2021-01-26 16:39:47 +0900

    mvc 연습문제

---
+++ ex1_springex/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,18 @@
+package com.javalec.ex;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		MyCalculator myCalculator = my MyCalculator();
+		myCalculator.setCalculator(new Calculator());
+		
+		myCalculator.setFirstNum();
+		myCalculator.setSecondNum();
+		
+		myCalculator.add();
+		myCalculator.sub();
+		myCalculator.mul();
+		myCalculator.div();
+	}
+}
